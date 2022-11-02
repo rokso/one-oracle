0bd3aaf40b35b931d097902db07fa2b9f548067b
Virendra Patidar <virendra@bloq.com>
2022-11-02 17:06:26 +0530

    ReDeployed oracle contrats for mainnet

--- tasks/create-release.js
+++ tasks/create-release.js
@@ -1,14 +1,12 @@
 const fs = require('fs')
 const _ = require('lodash')
-const compareVersions = require('compare-versions')
 
 const readFileAsJson = (fileName) => JSON.parse(fs.readFileSync(fileName).toString())
 
 const getAddress = (fileName) => readFileAsJson(fileName).address
 
 // Return pool deployment name and address
-function getDeploymentData() {
-  const network = hre.network.name
+function getDeploymentData(network) {
   const networkDir = `./deployments/${network}`
   const data = fs.readdirSync(networkDir).map(function (fileName) {
     if (fileName.includes('.json')) {
@@ -21,64 +19,26 @@ function getDeploymentData() {
   return _.merge(...data)
 }
 
-function getPreviousRelease() {
-  let releases = fs.readdirSync('releases')
-  if (releases.length) {
-    if (releases[0] === '.DS_Store') {
-      releases.shift() // delete first element, generally found on mac machine.
-    }
-    releases = releases.sort(compareVersions)
-    const prevRelease = releases[releases.length - 1]
-    const preReleaseFile = `releases/${prevRelease}/contracts.json`
-    if (fs.existsSync(preReleaseFile)) {
-      return readFileAsJson(preReleaseFile)
-    }
-  }
-  return {}
-}
-
 /* eslint-disable no-param-reassign */
-task('create-release', 'Create release file from deploy data')
-  .addParam('release', 'Vesper release semantic version, i.e 1.2.3')
-  .setAction(async function ({ release }) {
-    const network = hre.network.name
-    
+task('create-release', 'Create release file from deploy data').setAction(async function () {
+  const releaseDir = 'releases/'
+  const releaseFile = `${releaseDir}/contracts.json`
+
+  let releaseData = {}
+
+  if (!fs.existsSync(releaseDir)) {
+    fs.mkdirSync(releaseDir, {recursive: true})
+  }
+
+  ['avalanche', 'bsc', 'mainnet', 'polygon'].forEach((network) => {
     // Read deployment name and address
-    const deployData = getDeploymentData()
+    const deployData = getDeploymentData(network)
 
-    const releaseDir = `releases/${release}`
-    const releaseFile = `${releaseDir}/contracts.json`
-
-    // Get previous release data
-    const prevReleaseData = getPreviousRelease()
-    let releaseData = {}
-
-    // If last stored release is same as current release
-    if (prevReleaseData.version === release) {
-      // Update release with new deployment
-      releaseData = prevReleaseData
-    } else {
-      // If this is new release
-      // Create new release directory if doesn't exist
-      if (!fs.existsSync(releaseDir)) {
-        fs.mkdirSync(releaseDir, { recursive: true })
-      }
-      // Copy data from previous release
-      releaseData = prevReleaseData
-      // Update release version
-      releaseData.version = release
-    }
-    // We might have new network in this deployment, if not exist add empty network
-    if (!releaseData.networks) {
-      releaseData.networks = {}
-      releaseData.networks[network] = {}
-    } else if (!releaseData.networks[network]) {
-      releaseData.networks[network] = {}
-    }
-    // Update pool data with latest deployment
-    releaseData.networks[network] = deployData
-    // Write release data into file
-    fs.writeFileSync(releaseFile, JSON.stringify(releaseData, null, 2))
+    // Update latest deployment
+    releaseData[network] = deployData
   })
+  // Write release data into file
+  fs.writeFileSync(releaseFile, JSON.stringify(releaseData, null, 2))
+})
 
 module.exports = {}
