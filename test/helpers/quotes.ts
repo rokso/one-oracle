c43caf6e330ea138b744bdae7be676181bd4a6d9
Virendra Patidar <virendra@bloq.com>
2023-02-13 22:37:37 +0400

    Deployment for optimism chain

--- test/helpers/quotes.ts
+++ test/helpers/quotes.ts
@@ -76,6 +76,9 @@ const Quote = {
   mumbai: {MATIC_USD: parseEther('0.84'), ETH_USD: parseEther('1,359')},
   optimism: {
     ETH_USD: parseEther('1,543'),
+    USDC_USD: parseEther('1'),
+    DAI_USD: parseEther('1'),
+    OP_USD: parseEther('2.3'),
   }
 }
 export default Quote
