0acc600387d61998c631e73eade07befa6e39f84
Virendra Patidar <virendra@bloq.com>
2023-02-21 17:52:03 +0400

    Added SUSD LP Oracle

--- test/helpers/quotes.ts
+++ test/helpers/quotes.ts
@@ -75,10 +75,10 @@ const Quote = {
   },
   mumbai: {MATIC_USD: parseEther('0.84'), ETH_USD: parseEther('1,359')},
   optimism: {
-    ETH_USD: parseEther('1,663'),
+    ETH_USD: parseEther('1,675'),
     USDC_USD: parseEther('1'),
     DAI_USD: parseEther('1'),
-    OP_USD: parseEther('2.7'),
+    OP_USD: parseEther('2.5'),
   }
 }
 export default Quote
