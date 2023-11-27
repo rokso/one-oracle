c483e53ed0a9135283d367baccd350850ea408b7
Marcelo Morgado <cmarcelom@gmail.com>
2023-11-27 20:48:28 +0000

    Fix sFrxEth Oracle

--- contracts/periphery/tokens/SFraxEthTokenOracle.sol
+++ contracts/periphery/tokens/SFraxEthTokenOracle.sol
@@ -10,13 +10,14 @@ import "../../interfaces/periphery/ITokenOracle.sol";
 /**
  * @title Oracle for `sFraxEthToken`
  * @dev Based on https://etherscan.deth.net/address/0x27942aFe4EcB7F9945168094e0749CAC749aC97B#code
+ * @dev `WETH/FRXETH` Curve pool's oracle is safer than `ETH/FRXETH`
  */
 contract SFraxEthTokenOracle is ITokenOracle {
     uint256 public constant MAX_FRXETH_PRICE = 1e18;
     uint256 public constant MIN_FRXETH_PRICE = 0.9e18;
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    IFrxEthStableSwap public constant ETH_FRXETH_CURVE_POOL =
-        IFrxEthStableSwap(0xa1F8A6807c402E4A15ef4EBa36528A3FED24E577);
+    IFrxEthStableSwap public constant WETH_FRXETH_CURVE_POOL =
+        IFrxEthStableSwap(0x9c3B46C0Ceb5B9e304FCd6D88Fc50f7DD24B31Bc);
     ISFrxEth public constant SFRXETH = ISFrxEth(0xac3E018457B222d93114458476f3E3416Abbe38F);
 
     /// @inheritdoc ITokenOracle
@@ -25,7 +26,7 @@ contract SFraxEthTokenOracle is ITokenOracle {
         uint256 _ethPriceInUsd = IOracle(msg.sender).getPriceInUsd(WETH);
 
         // FrxETH/ETH price
-        uint256 _frxEthPriceInEth = ETH_FRXETH_CURVE_POOL.price_oracle();
+        uint256 _frxEthPriceInEth = WETH_FRXETH_CURVE_POOL.price_oracle();
 
         if (_frxEthPriceInEth > MAX_FRXETH_PRICE) {
             _frxEthPriceInEth = MAX_FRXETH_PRICE;
