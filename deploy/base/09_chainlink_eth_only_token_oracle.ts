169fb47074080efef047f5b127e4c1e42c79d580
patidarmanoj10 <patidarmanoj@gmail.com>
2024-06-17 21:12:24 -0700

    vPool oracle deployment

---
+++ deploy/base/09_chainlink_eth_only_token_oracle.ts
@@ -0,0 +1,19 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types'
+import {DeployFunction} from 'hardhat-deploy/types'
+
+const ChainlinkEthOnlyTokenOracle = 'ChainlinkEthOnlyTokenOracle'
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const {getNamedAccounts, deployments} = hre
+  const {deploy} = deployments
+  const {deployer} = await getNamedAccounts()
+
+  await deploy(ChainlinkEthOnlyTokenOracle, {
+    from: deployer,
+    log: true,
+    args: [],
+  })
+}
+
+export default func
+func.tags = [ChainlinkEthOnlyTokenOracle]
