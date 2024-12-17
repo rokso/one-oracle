f456c96c3a50483433ba7364d36f52a1c1adccd3
Marcelo Morgado <cmarcelom@gmail.com>
2024-12-17 10:40:08 +0000

    Swell deployment

--- tasks/create-release.js
+++ tasks/create-release.js
@@ -30,7 +30,7 @@ task("create-release", "Create release file from deploy data").setAction(async f
     fs.mkdirSync(releaseDir, {recursive: true});
   }
 
-  ["avalanche", "mainnet", "polygon", "optimism", "base", "hemi"].forEach((network) => {
+  ["avalanche", "mainnet", "polygon", "optimism", "base", "hemi", "swell"].forEach((network) => {
     // Read deployment name and address
     const deployData = getDeploymentData(network);
 
